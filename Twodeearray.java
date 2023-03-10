f9c6bd2af4688dea5ebd9e4328c28b9b86a08a23
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-03-09 19:21:19 -0600

    Update Twodeearray.java

--- Twodeearray.java
+++ Twodeearray.java
@@ -1,8 +1,8 @@
 public class Twodeearray {
     int solution(int[] arr) {
-    //use two for loops that one for the rows(i) and j for the columns. This will be for the hourglass inside of the 2d array.
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    //use two for loops that one for the rows(i) and j for the columns. This will be for the 3x3 hourglass inside of the 2d array.
+    for (int i = 0; i <= 3; i++) {
+        for (int j = 0; j <= 3; j++) {
             //In order to get the sum of the integers in the hourglass, we would have to look at the chart for a 2d array to see its positional numbers and would have to create an hourglass shape from all its positions
             int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] 
                                    + arr[i+1][j+1] 
