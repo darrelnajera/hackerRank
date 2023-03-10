f6d8956a01680ec20bd649f64eeb0b070651e13a
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-03-10 15:44:49 -0600

    Update Loopsone.java

--- Loopsone.java
+++ Loopsone.java
@@ -12,8 +12,9 @@ import java.util.regex.*;
 public class Loopsone{
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
+        //in order to input several integers on one line similar to a string, we store the parse int trim method below to remove white space
         int N = Integer.parseInt(bufferedReader.readLine().trim());
+        //for loop prints out N times all numbers through 10 on each separate line
         for(int i = 1; i <= 10; i++){
             System.out.println(N + " x " + i + " = " + (N*i) );
         }
