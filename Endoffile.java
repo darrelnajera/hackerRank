db6874c6f7729dcebcce458d4893b81d4337588c
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-03-10 14:49:46 -0600

    Update Endoffile.java

--- Endoffile.java
+++ Endoffile.java
@@ -5,11 +5,12 @@ public class Endoffile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        //counter for each string on new line starting from number 1
         int count = 1;
        
 
-        
+        //the while loop stops working until new data(input) is inputted.
+        //count increases by 1
         while (scanner.hasNext()) {
             System.out.println(count + " " + scanner.nextLine());
             count++;
