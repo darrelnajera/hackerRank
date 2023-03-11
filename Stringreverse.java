8c494655aaa76928794c49ac3ffa9d6e0c99e2ed
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-03-10 18:11:51 -0600

    Update Stringreverse.java

--- Stringreverse.java
+++ Stringreverse.java
@@ -6,8 +6,12 @@ public class Stringreverse {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
+        //user input
         String input =scanner.nextLine();
+        //implement the reverse toString to reverse input and check if it is a palindrome
+        //store it into string palindrome
         String palindrome = new StringBuilder(input).reverse().toString();
+        //after reversed, if input equals a palindrome print out yes, otherwise print no
         if(input.equals(palindrome)) {
             System.out.println("Yes");
         } else {
