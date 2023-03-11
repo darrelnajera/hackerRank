3bdaf09d734e2ed54f3c7b4f767bcfb7d06e6cd9
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-03-10 18:01:43 -0600

    Update Staticinitializerblock.java

--- Staticinitializerblock.java
+++ Staticinitializerblock.java
@@ -6,17 +6,28 @@ public class Staticinitializerblock {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        int base = scanner.nextInt();
-        int height = scanner.nextInt();
-        if (base > 0 && height > 0){
-            System.out.println(findArea(base,height));
-        } else {
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-        }
+        //call method use 4 and 7 as an example
+        findArea(4,7);
+       
     }
     
     public static int findArea(int base, int height){
-        return base * height;
+        //implement scanner in method
+        Scanner scanner = new Scanner(System.in);
+        //store value for area
+        int area = 0;
+        //user input for base and height
+        int base = scanner.nextInt();
+        int height = scanner.nextInt();
+        //if input involves postive numbers, area equals base times height
+        if (base > 0 && height > 0){
+            area = base * height;
+        //print out exception error if any numbers are negative 
+        } else {
+            
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        return area
     }
 }
 
