d14feb0b2d412941da3da023e10703005ede18c3
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-23 17:33:28 -0500

    Add files via upload

---
+++ Validusernameregularexpression.java
@@ -0,0 +1,10 @@
+public class Validusernameregularexpression {
+
+class UsernameValidator {
+    //use regex. a-zA-z = any lower or upper case letters and 7-29 is the range in amount of characters
+    public static final String regularExpression = "^[a-zA-Z]\\w{7,29}$";
+}
+
+
+    
+}
