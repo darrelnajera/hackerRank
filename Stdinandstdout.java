47202c9d6978f671dc1890868993774194d05640
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-22 20:43:43 -0500

    Add files via upload

---
+++ Stdinandstdout.java
@@ -0,0 +1,17 @@
+public class Stdinandstdout {
+    import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        // Complete this line
+        // Complete this line
+    }
+}
+}
