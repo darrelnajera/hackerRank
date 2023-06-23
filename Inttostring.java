ef092439a597753241be52ccb29f38d78b80d7ef
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-23 15:38:47 -0500

    Add files via upload

---
+++ Inttostring.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.security.*;
+public class Inttostring {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //solution is to use Integer.toString to turn int n into a string
+    String s = Integer.toString(n);
+
+}
+ }
+}
