f117838ab69164fec84902dbf74617281a4d1d51
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-25 17:21:11 -0500

    Add files via upload

---
+++ Welcometojava.java
@@ -0,0 +1,8 @@
+public class Welcometojava {
+
+    public static void main(String[] args) {
+        //return messages using sout
+      System.out.println("Hello, World.");
+System.out.println("Hello, Java.");  
+    }
+}
