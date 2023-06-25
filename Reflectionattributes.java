f293f469ff22a276a947b251d7e2a37ae1f93677
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-25 16:47:30 -0500

    Update Reflectionattributes.java

--- Reflectionattributes.java
+++ Reflectionattributes.java
@@ -5,9 +5,9 @@ public class Reflectionattributes {
         //use get.declared methods to return the method object
         Method[] methods = student.getDeclaredMethods();
         
-        
+        //create arraylist
         ArrayList<String> methodList = new ArrayList<>();
-        //for each loop to iterate through Method class
+        //for each loop to iterate through Arraylist
         for(Method method : methods){
             //for every name, add them to arraylist
             methodList.add(method.getName());
