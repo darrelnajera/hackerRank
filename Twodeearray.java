5521782adbcf9a44cffe7d51e6604a521711dccc
darrelnajera <103217867+darrelnajera@users.noreply.github.com>
2023-06-28 16:04:02 -0500

    Add files via upload

--- Twodeearray.java
+++ Twodeearray.java
@@ -1,18 +1,57 @@
-public class Twodeearray {
-    int solution(int[] arr) {
-    //use two for loops that one for the rows(i) and j for the columns. This will be for the 3x3 hourglass inside of the 2d array.
-    for (int i = 0; i <= 3; i++) {
-        for (int j = 0; j <= 3; j++) {
-            //In order to get the sum of the integers in the hourglass, we would have to look at the chart for a 2d array to see its positional numbers and would have to create an hourglass shape from all its positions
-            int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] 
-                                   + arr[i+1][j+1] 
-                    + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
-            
-        }
-    }
-    //print out the sum of all the integers in the hourglass.
-    System.out.println(sum);
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
+
+
+public class Twodeearray {
+    static int hourglassSum(int[][] array) {
+        int rows = array.length;
+        int columns = array[0].length;
+        
+        int maxSum = Integer.MIN_VALUE;
+        //use two for loops for rows and columns. We would have to subtract 2 from rows and columns to avoid going out of bounds searching for non existent space to create hourglasses
+        for(int i = 0; i < rows-2; i++){
+            for(int j = 0; j < columns-2; j++) {
+                //add together all possible values that form an hourglass in the 2-d array. move on to next hourglass
+                int currentSum = array[i][j] + array[i][j+1] + array[i][j+2] 
+                                    + array[i+1][j+1] + 
+                  array[i+2][j] + array[i+2][j+1] + array[i+2][j+2];
+                //values we will compare to get the greatest between the sum of current hourglass we are on and thhe rest
+                maxSum = Math.max(maxSum,currentSum);
+                
+                
+            }
+        }
+        //return largest value between the two
+        return maxSum;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
     }
 }
-}
+
